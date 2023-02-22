0f72b72aeaa9d385a474f82d9281774499c0d883
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2023-02-22 18:27:45 +0530

    Update main.js

--- .vscode/main.js
+++ .vscode/main.js
@@ -1,5 +1,3 @@
-/// <reference path="c:/Users/swamp/.vscode/extensions/nur.script-0.2.1/@types/api.global.d.ts" />
-
 function activate(_context) {
    window.showInformationMessage('Hello, World!');
 }
