6e4a9230e196a97aa9b8520bbf6f3167e1d9b510
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2023-05-20 15:05:36 +0530

    installation process

--- src/main.py
+++ src/main.py
@@ -7,9 +7,6 @@ gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 gi.require_version('Notify', '0.7')
 from gi.repository import Gtk,Gio, Gdk, Notify
-
-
-
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
