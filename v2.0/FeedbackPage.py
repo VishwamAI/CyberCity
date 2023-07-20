d98887811e8df40c12e09757d6a1c9b1da7f9279
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2023-07-21 04:21:00 +0530

    Update
    Signed-off-by: swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>

---
+++ v2.0/FeedbackPage.py
@@ -0,0 +1,46 @@
+from gi.repository import Gtk
+import webbrowser
+from Page import Page
+class FeedbackPage(Page):
+    def __init__(self, back_label):
+        super().__init__(back_label)
+
+        self.grid = Gtk.Grid()
+        self.append(self.grid)
+
+        tools = ["Email", "LinkedIn", "Facebook", "Youtube"]
+        for i, tool in enumerate(tools):
+            btn = Gtk.Button(label=tool)
+            btn.get_style_context().add_class("circular")
+            btn.connect("clicked", self.open_page, tool)
+            self.grid.attach(btn, i % 3, i // 3, 1, 1)
+            btn.set_size_request(200, 200)
+
+            btn.set_hexpand(True)
+            btn.set_vexpand(True)
+            btn.set_halign(Gtk.Align.CENTER)
+            btn.set_valign(Gtk.Align.CENTER)
+
+    def open_page(self, button, page_name):
+        button.get_style_context().add_class('clicked')
+        
+        if page_name == "Email":
+            self.contact_email(button)
+        elif page_name == "LinkedIn":
+            self.open_linkedin(button)
+        elif page_name == "Facebook":
+            self.open_facebook(button)
+        elif page_name == "Youtube":
+            self.open_youtube(button)
+
+    def contact_email(self, button):
+        webbrowser.open("mailto:exploit0xffff@gmail.com")
+
+    def open_linkedin(self, button):
+        webbrowser.open("https://www.linkedin.com/company/exploit0xfffff")
+
+    def open_facebook(self, button):
+        webbrowser.open("https://www.facebook.com/Exploit0xffffff")
+
+    def open_youtube(self, button):
+        webbrowser.open("https://www.youtube.com/channel/UC4JXxQqQqXK4QHhQgqEeYjQ")
---
+++ v2.0/LinuxPage.py
@@ -0,0 +1,33 @@
+from gi.repository import Gtk
+from Page import Page
+class LinuxPage(Page):
+    def __init__(self, back_label):
+        super().__init__(back_label)
+        self.grid = Gtk.Grid()
+
+        self.grid.set_row_spacing(20)
+        self.grid.set_column_spacing(10)
+
+        scroll = Gtk.ScrolledWindow()
+        scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scroll.set_child(self.grid)
+
+        self.append(scroll)
+
+        tools = ["Information Gathering","Vulnerability Analysis", "WebApplication", "DataBase-Assessment","Password Attacks","Wireless Attacks","Reverse Engineering","Exploration Tools","Sniffing Tools","Post Exploitation","Forensics","Reporting Tools","Social Engineering Toolkit","System Services"]
+        for i, tool in enumerate(tools):
+            btn = Gtk.Button(label=tool)
+            btn.get_style_context().add_class("circular")
+            btn.connect("clicked", self.open_page, tool)
+            self.grid.attach(btn, i % 3, i // 3, 1, 1)
+            btn.set_size_request(200, 200)
+
+            btn.set_hexpand(True)
+            btn.set_vexpand(True)
+            btn.set_halign(Gtk.Align.CENTER)
+            btn.set_valign(Gtk.Align.CENTER)
+
+    def open_page(self, button, page_name):
+        button.get_style_context().add_class('clicked')
+        main_window = self.get_root()
+        main_window.open_page(button, page_name)
